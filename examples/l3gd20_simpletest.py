036765015baaffddc9a8de20eb431575129ab395
mrmcwethy <mrmcwethy@yahoo.com>
2018-06-18 10:11:12 -0700

    Added comments to the examples files.

--- examples/l3gd20_simpletest.py
+++ examples/l3gd20_simpletest.py
@@ -3,7 +3,9 @@ from board import SCL, SDA
 import busio
 import adafruit_l3gd20
 
+# define the I2C wires
 I2C = busio.I2C(SCL, SDA)
+# initialize the device
 SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C)
 
 while True:
