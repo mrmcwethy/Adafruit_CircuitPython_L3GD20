ef2476f62632be5a87e290aeffa848d9761b5b0a
dherrada <dylan.herrada@gmail.com>
2020-06-25 15:42:04 -0400

    Better documentation in simpletest, new example: write_registers

--- examples/l3gd20_simpletest.py
+++ examples/l3gd20_simpletest.py
@@ -5,7 +5,16 @@ import adafruit_l3gd20
 
 # Hardware I2C setup:
 I2C = busio.I2C(board.SCL, board.SDA)
+# Initializes L3GD20 object using default range, 250dps
 SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C)
+# Initialize L3GD20 object using a different range.
+#SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C, rng=adafruit_l3gd20.L3DS20_RANGE_2000DPS)
+
+# Possible values for rng are:
+# adafruit_l3gd20.L3DS20_Range_250DPS, 250 degrees per second. Default range
+# adafruit_l3gd20.L3DS20_Range_500DPS, 500 degrees per second
+# adafruit_l3gd20.L3DS20_Range_2000DPS, 2000 degrees per second
+
 
 # Hardware SPI setup:
 # import digitalio
---
+++ examples/l3gd20_write_registers.py
@@ -0,0 +1,19 @@
+import time
+import board
+import busio
+import adafruit_l3gd20
+
+# Hardware I2C setup:
+I2C = busio.I2C(board.SCL, board.SDA)
+SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C, rng=adafruit_l3gd20.L3DS20_RANGE_2000DPS)
+
+# Update the chip's register 0x20 (CTRL_REG1)  with the value 0xBF
+SENSOR.write_register(0x20, 0xBF)
+
+# This sets the output data rate to 380Hz and keeps everything else on their default modes
+# For more information about CTRL_REG1, see section 7.2 of the datasheet
+
+while True:
+    print("Angular Momentum (rad/s): {}".format(SENSOR.gyro))
+    print()
+    time.sleep(1)
