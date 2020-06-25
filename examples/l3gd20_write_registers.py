45c31c2ff0ced28e0d2efc3ee82dce19ea61f2cf
dherrada <dylan.herrada@gmail.com>
2020-06-25 15:52:56 -0400

    Tweaked Hz in comment

--- examples/l3gd20_write_registers.py
+++ examples/l3gd20_write_registers.py
@@ -7,11 +7,11 @@ import adafruit_l3gd20
 I2C = busio.I2C(board.SCL, board.SDA)
 SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C, rng=adafruit_l3gd20.L3DS20_RANGE_2000DPS)
 
-# Update the chip's register 0x20 (CTRL_REG1)  with the value 0xBF
+# Update the chip's register 0x20 (CTRL1)  with the value 0xBF
 SENSOR.write_register(0x20, 0xBF)
 
-# This sets the output data rate to 380Hz and keeps everything else on their default modes
-# For more information about CTRL_REG1, see section 7.2 of the datasheet
+# This sets the output data rate to 400Hz and keeps everything else on their default modes
+# For more information about CTRL1, see section 7.2 of the datasheet
 
 while True:
     print("Angular Momentum (rad/s): {}".format(SENSOR.gyro))
