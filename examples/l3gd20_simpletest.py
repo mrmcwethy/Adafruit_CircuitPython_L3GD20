4c47e5581f6aa043eecd8434fc8f48624df5092c
dherrada <dylan.herrada@adafruit.com>
2020-06-29 12:04:04 -0400

    Added support for setting data rate

--- examples/l3gd20_simpletest.py
+++ examples/l3gd20_simpletest.py
@@ -6,21 +6,27 @@ import adafruit_l3gd20
 # Hardware I2C setup:
 I2C = busio.I2C(board.SCL, board.SDA)
 # Initializes L3GD20 object using default range, 250dps
-SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C)
-# Initialize L3GD20 object using a different range.
-# SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C, rng=adafruit_l3gd20.L3DS20_RANGE_2000DPS)
+# SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C)
+# Initialize L3GD20 object using a custom range and output data rate (ODR).
+SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C, rng=adafruit_l3gd20.L3DS20_RANGE_500DPS, rate=adafruit_l3gd20.L3DS20_RATE_200Hz)
 
 # Possible values for rng are:
 # adafruit_l3gd20.L3DS20_Range_250DPS, 250 degrees per second. Default range
 # adafruit_l3gd20.L3DS20_Range_500DPS, 500 degrees per second
 # adafruit_l3gd20.L3DS20_Range_2000DPS, 2000 degrees per second
 
+# Possible values for rate are:
+# adafruit_l3gd20.L3DS20_RATE_100Hz, 100Hz data rate. Default data rate
+# adafruit_l3gd20.L3DS20_RATE_200Hz, 200Hz data rate
+# adafruit_l3gd20.L3DS20_RATE_400Hz, 400Hz data rate
+# adafruit_l3gd20.L3DS20_RATE_800Hz, 800Hz data rate
 
 # Hardware SPI setup:
 # import digitalio
 # CS = digitalio.DigitalInOut(board.D5)
 # SPIB = busio.SPI(board.SCK, board.MOSI, board.MISO)
 # SENSOR = adafruit_l3gd20.L3GD20_SPI(SPIB, CS)
+# SENSOR = adafruit_l3gd20.L3GD20_I2C(SPIB, CS, rng=adafruit_l3gd20.L3DS20_RANGE_500DPS, rate=adafruit_l3gd20.L3DS20_RATE_200Hz)
 
 while True:
     print("Angular Momentum (rad/s): {}".format(SENSOR.gyro))
