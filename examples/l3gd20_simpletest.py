6a65910bffc49b357ea10a0b7e87422b7c47f369
dherrada <dylan.herrada@adafruit.com>
2020-06-29 12:49:49 -0400

    More formatting

--- examples/l3gd20_simpletest.py
+++ examples/l3gd20_simpletest.py
@@ -8,7 +8,9 @@ I2C = busio.I2C(board.SCL, board.SDA)
 # Initializes L3GD20 object using default range, 250dps
 SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C)
 # Initialize L3GD20 object using a custom range and output data rate (ODR).
-# SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C, rng=adafruit_l3gd20.L3DS20_RANGE_500DPS, rate=adafruit_l3gd20.L3DS20_RATE_200HZ)
+# SENSOR = adafruit_l3gd20.L3GD20_I2C(
+#    I2C, rng=adafruit_l3gd20.L3DS20_RANGE_500DPS, rate=adafruit_l3gd20.L3DS20_RATE_200HZ
+# )
 
 # Possible values for rng are:
 # adafruit_l3gd20.L3DS20_Range_250DPS, 250 degrees per second. Default range
@@ -26,7 +28,12 @@ SENSOR = adafruit_l3gd20.L3GD20_I2C(I2C)
 # CS = digitalio.DigitalInOut(board.D5)
 # SPIB = busio.SPI(board.SCK, board.MOSI, board.MISO)
 # SENSOR = adafruit_l3gd20.L3GD20_SPI(SPIB, CS)
-# SENSOR = adafruit_l3gd20.L3GD20_I2C(SPIB, CS, rng=adafruit_l3gd20.L3DS20_RANGE_500DPS, rate=adafruit_l3gd20.L3DS20_RATE_200HZ)
+# SENSOR = adafruit_l3gd20.L3GD20_I2C(
+#    SPIB,
+#    CS,
+#    rng=adafruit_l3gd20.L3DS20_RANGE_500DPS,
+#    rate=adafruit_l3gd20.L3DS20_RATE_200HZ,
+# )
 
 while True:
     print("Angular Momentum (rad/s): {}".format(SENSOR.gyro))
